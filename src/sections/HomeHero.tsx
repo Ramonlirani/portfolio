54c9ee6d68bec7d085c03a00d2418d968d214295
ramonlirani <ramonlirani@hotmail.com>
2023-12-14 22:13:28 -0300

    fix: changed sections home

--- src/sections/HomeHero.tsx
+++ src/sections/HomeHero.tsx
@@ -9,58 +9,64 @@ import ScrollToTop from "../components/ScrollToTop";
 
 //mocks
 import { HERO_TITLES } from "../mocks/profile";
+import Image from "next/image";
+import photo from "@/assets/image/photoperfil.jpeg";
 
 export default function HomeHero() {
-   const { isScroll, jumpToDown } = useContext(ScrollContext);
+   const { isScroll, jumpToDown }: any = useContext(ScrollContext);
 
    return (
-      <section className="container mx-auto mt-3 flex max-h-screen min-h-screen flex-col items-center justify-around px-5 md:px-1">
-         <div className="z-50 flex flex-col items-center lg:space-y-10 space-y-5">
-            <img
-               src="https://media.licdn.com/dms/image/D4D03AQHAnwoUnM_0xA/profile-displayphoto-shrink_800_800/0/1702350427487?e=1707955200&v=beta&t=cVWsIr5jlVwlEpIN9HLGFb-bRJIyCnnXioEuPDluaD0"
-               className="w-52 object-contain lg:w-64 rounded-full"
-               alt="Logo"
-            />
-
-            <h1 className="z-50 font-lato text-2xl font-bold sm:text-4xl md:font-semibold">
-               HI, Im {""}
-               <span className="text-primary-500">
-                  <Typewriter
-                     words={HERO_TITLES}
-                     loop={false}
-                     cursor
-                     cursorStyle=" "
-                     typeSpeed={70}
-                     deleteSpeed={50}
-                     delaySpeed={1000}
-                  />
-               </span>
-            </h1>
-            <p className="text-center text-sm sm:text-base md:w-3/5">
-               Highly motivated Full Stack Developer with a passion for
-               creating, with experience in designing, developing and
-               maintaining web applications using technologies such as
-               JavaScript, React and Node.js.
-            </p>
+      <>
+         <section className="container mx-auto mt-3 flex max-h-screen min-h-screen flex-col items-center justify-around px-5 md:px-1">
+            <div className="z-50 flex flex-col items-center lg:space-y-10 space-y-5">
+               <Image
+                  src={photo}
+                  className="w-52 object-contain lg:w-64 rounded-full"
+                  alt="Logo"
+               />
+               <div>
+                  <h1 className="z-50 font-lato text-2xl font-bold sm:text-4xl md:font-semibold">
+                     HI, Im {""}
+                     <span className="text-primary-500">
+                        <Typewriter
+                           words={HERO_TITLES}
+                           loop={false}
+                           cursor
+                           cursorStyle=" "
+                           typeSpeed={70}
+                           deleteSpeed={50}
+                           delaySpeed={1000}
+                        />
+                     </span>
+                  </h1>
+               </div>
+               <p className="text-center text-sm sm:text-base md:w-3/5">
+                  Brazilian guy Full Stack Developer, I&apos;m highly motivated
+                  and passionate about creating. I have extensive experience in
+                  designing, developing and maintaining web applications using
+                  the latest technologies on the market.
+               </p>
+            </div>
             <SocialLinks />
-         </div>
-         <div
-            id="mouse-scroll"
-            className={`ml-4 hidden cursor-pointer transition-all duration-200 ${
-               isScroll ? "opacity-0" : ""
-            }`}
-            onClick={jumpToDown}
-         >
-            <div className="mouse d border-2 border-solid border-primary-500 dark:border-primary-500">
-               <div className="mouse-in bg-primary-500 dark:bg-primary-500" />
+
+            <div
+               id="mouse-scroll"
+               className={`ml-4 hidden cursor-pointer transition-all duration-200 ${
+                  isScroll ? "opacity-0" : ""
+               }`}
+               onClick={jumpToDown}
+            >
+               <div className="mouse d border-2 border-solid border-primary-500 dark:border-primary-500">
+                  <div className="mouse-in bg-primary-500 dark:bg-primary-500" />
+               </div>
+               <div className="mt-3">
+                  <span className="down-arrow-1 border-r-2 border-b-2 border-solid border-primary-500 dark:border-primary-500" />
+                  <span className="down-arrow-2 border-r-2 border-b-2 border-solid border-primary-500 dark:border-primary-500" />
+                  <span className="down-arrow-3 border-r-2 border-b-2 border-solid border-primary-500 dark:border-primary-500" />
+               </div>
             </div>
-            <div className="mt-3">
-               <span className="down-arrow-1 border-r-2 border-b-2 border-solid border-primary-500 dark:border-primary-500" />
-               <span className="down-arrow-2 border-r-2 border-b-2 border-solid border-primary-500 dark:border-primary-500" />
-               <span className="down-arrow-3 border-r-2 border-b-2 border-solid border-primary-500 dark:border-primary-500" />
-            </div>
-         </div>
-         <ScrollToTop />
-      </section>
+            <ScrollToTop />
+         </section>
+      </>
    );
 }

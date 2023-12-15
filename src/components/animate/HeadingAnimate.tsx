d741d0346ad5087ffa5190e8c9540de3a17d54c4
ramonlirani <ramonlirani@hotmail.com>
2023-12-14 22:12:07 -0300

    feat: news components animated

--- src/components/animate/HeadingAnimate.tsx
+++ src/components/animate/HeadingAnimate.tsx
@@ -8,16 +8,18 @@ interface HeadingAnimate {
 
 function HeadingAnimate({ children, amount = 0.5 }: HeadingAnimate) {
    return (
-      <motion.div
-         initial={{ opacity: 0, scale: 0.7 }}
-         whileInView={{ opacity: 1, scale: 1 }}
-         transition={{
-            duration: 0.6,
-         }}
-         viewport={{ once: false, amount: amount }}
-      >
-         {children}
-      </motion.div>
+      <>
+         <motion.div
+            initial={{ opacity: 0, scale: 0.7 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{
+               duration: 0.6,
+            }}
+            viewport={{ once: false, amount: amount }}
+         >
+            {children}
+         </motion.div>
+      </>
    );
 }
 
--- src/components/animate/LoadAnimate.tsx
+++ src/components/animate/LoadAnimate.tsx
@@ -8,16 +8,18 @@ interface LoadAnimateProps {
 
 function LoadAnimate({ children, amount = 0.5 }: LoadAnimateProps) {
    return (
-      <motion.div
-         initial={{ opacity: 0, x: -50 }}
-         whileInView={{ opacity: 1, x: 0 }}
-         transition={{
-            duration: 0.6,
-         }}
-         viewport={{ once: false, amount: amount }}
-      >
-         {children}
-      </motion.div>
+      <>
+         <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{
+               duration: 0.6,
+            }}
+            viewport={{ once: false, amount: amount }}
+         >
+            {children}
+         </motion.div>
+      </>
    );
 }
 
